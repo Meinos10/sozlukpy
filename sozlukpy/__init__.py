ea73e96141125820b30ff028f7dbe5e2f30342b4
ReWoxi <64690769+Meinos10@users.noreply.github.com>
2022-12-03 20:17:14 +0300

    Update __init__.py

--- sozlukpy/__init__.py
+++ sozlukpy/__init__.py
@@ -4,7 +4,7 @@
 
 import requests as req
 
-class Sozluk():
+class Sozluk:
     header = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
     url = "https://sozluk.gov.tr/gts?ara="
     def anlam(self, kelime: str):
@@ -15,30 +15,53 @@ class Sozluk():
             return anlam["error"]
         return anlam[0]["anlamlarListe"][0]["anlam"]
 
-    def anlamlar(self, kelime: str):
+    def anlamlar(self, kelime: str, limit: int=100):
         header = self.header
         url = self.url
         anlamlar = req.get(url+str(kelime.lower()), headers=header).json()
         if anlamlar == {"error":"Sonuç bulunamadı"}:
             return anlamlar["error"]
         text = ""
+        n = 1
         text+=str(kelime.upper())+"\n\n"
         for t in anlamlar[0]["anlamlarListe"]:
-            text+=str(t["anlam_sira"])+" - "+str(t["anlam"])+"\n"
+            if n <= limit:
+                text+=str(t["anlam_sira"])+" - "+str(t["anlam"])+"\n"
+                n+=1
         return text
 
-    def ornekler(self, kelime: str):
+    def ornekler(self, kelime: str, limit: int=100):
         header = self.header
         url = self.url
         ornekler = req.get(url+str(kelime.lower()), headers=header).json()
         if ornekler == {"error":"Sonuç bulunamadı"}:
             return ornekler["error"]
         text = ""
+        n = 1
         text+=str(kelime.upper())+"\n\n"
         for t in ornekler[0]["anlamlarListe"]:
             try:
                 for o in t["orneklerListe"]:
-                    text+="- "+o["ornek"]+" | "+o["yazar"][0]["tam_adi"]+"\n"
+                    if n <= limit:
+                        text+="- "+o["ornek"]+" | "+o["yazar"][0]["tam_adi"]+"\n"
+                        n+=1
             except KeyError:
                 pass
         return text
+    
+    def atasozleri(self, kelime: str, limit: int=100):
+        header = self.header
+        url = self.url
+        ornekler = req.get(url+str(kelime.lower()), headers=header).json()
+        if ornekler == {"error":"Sonuç bulunamadı"}:
+            return ornekler["error"]
+        text = ""
+        n = 1
+        text+=str(kelime.upper())+"\n\n"
+        for t in ornekler[0]["atasozu"]:
+            if n <= limit:
+                text+=str(n)+" - "+t["madde"]+"\n"
+                n+=1
+        return text
+    
+    
