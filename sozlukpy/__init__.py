8427a55310fec98717826f33e0a17eb53f75aeb4
ReWoxi <64690769+Meinos10@users.noreply.github.com>
2023-04-17 22:40:56 +0300

    Update __init__.py

--- sozlukpy/__init__.py
+++ sozlukpy/__init__.py
@@ -2,11 +2,14 @@
 
 # https://github.com/Meinos10/sozlukpy
 
+
 import requests as req
 
 class Sozluk:
     header = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
     url = "https://sozluk.gov.tr/gts?ara="
+
+
     def anlam(self, kelime: str):
         header = self.header
         url = self.url
@@ -21,13 +24,12 @@ class Sozluk:
         anlamlar = req.get(url+str(kelime.lower()), headers=header).json()
         if anlamlar == {"error":"Sonuç bulunamadı"}:
             return anlamlar["error"]
-        text = ""
+        text = {}
         n = 1
-        text+=str(kelime.upper())+"\n\n"
         for t in anlamlar[0]["anlamlarListe"]:
             if n <= limit:
-                text+=str(t["anlam_sira"])+" - "+str(t["anlam"])+"\n"
-                n+=1
+                text[str(n)] = t["anlam"]
+                n += 1
         return text
 
     def ornekler(self, kelime: str, limit: int=100):
@@ -36,14 +38,13 @@ class Sozluk:
         ornekler = req.get(url+str(kelime.lower()), headers=header).json()
         if ornekler == {"error":"Sonuç bulunamadı"}:
             return ornekler["error"]
-        text = ""
+        text = {}
         n = 1
-        text+=str(kelime.upper())+"\n\n"
         for t in ornekler[0]["anlamlarListe"]:
             try:
                 for o in t["orneklerListe"]:
                     if n <= limit:
-                        text+="- "+o["ornek"]+" | "+o["yazar"][0]["tam_adi"]+"\n"
+                        text[str(n)] = o["ornek"]
                         n+=1
             except KeyError:
                 pass
@@ -55,13 +56,11 @@ class Sozluk:
         ornekler = req.get(url+str(kelime.lower()), headers=header).json()
         if ornekler == {"error":"Sonuç bulunamadı"}:
             return ornekler["error"]
-        text = ""
+        text = {}
         n = 1
-        text+=str(kelime.upper())+"\n\n"
         for t in ornekler[0]["atasozu"]:
             if n <= limit:
-                text+=str(n)+" - "+t["madde"]+"\n"
+                text[str(n)] = t["madde"]
                 n+=1
         return text
-    
-    
+
